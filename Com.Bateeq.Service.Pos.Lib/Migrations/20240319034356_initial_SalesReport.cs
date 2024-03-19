f56d0b90968ffe0b520b10e30a59f994534638c4
achensetyawati <achenzetya@gmail.com>
2024-03-19 11:41:17 +0700

    add sales report table

---
+++ Com.Bateeq.Service.Pos.Lib/Migrations/20240319034356_initial_SalesReport.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Bateeq.Service.Pos.Lib.Migrations
+{
+    public partial class initial_SalesReport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
